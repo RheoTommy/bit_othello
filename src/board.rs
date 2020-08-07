2dbfa0be3b66dc8267a5c8aac9bc01479402be85
RheoTommy <tommyrheo@gmail.com>
2020-08-07 18:45:38 +0900

    着手可能盤面前列挙関数とテスト

--- src/board.rs
+++ src/board.rs
@@ -62,8 +62,137 @@ impl Board {
         }
     }
 
-    pub fn coordinate_to_bit(co: Coordinate) -> BitBoard {
+    fn coordinate_to_bit(co: Coordinate) -> BitBoard {
         let (i, j) = co;
         1 << (63 - i * 8 - j)
+    }
+
+    pub fn make_legal_board(&self) -> BitBoard {
+        let horizontal_watch_board = self.opponent_board & 0x7e7e7e7e7e7e7e7e;
+        let vertical_watch_board = self.opponent_board & 0x00FFFFFFFFFFFF00;
+        let all_side_watch_board = self.opponent_board & 0x007e7e7e7e7e7e00;
+
+        // 空きマス列挙
+        let blank_board = !(self.player_board | self.opponent_board);
+
+        let mut tmp;
+
+        let mut legal_board;
+
+        // 左
+        tmp = horizontal_watch_board & (self.player_board << 1);
+        tmp |= horizontal_watch_board & (tmp << 1);
+        tmp |= horizontal_watch_board & (tmp << 1);
+        tmp |= horizontal_watch_board & (tmp << 1);
+        tmp |= horizontal_watch_board & (tmp << 1);
+        tmp |= horizontal_watch_board & (tmp << 1);
+        legal_board = blank_board & (tmp << 1);
+
+        // 右
+        tmp = horizontal_watch_board & (self.player_board >> 1);
+        tmp |= horizontal_watch_board & (tmp >> 1);
+        tmp |= horizontal_watch_board & (tmp >> 1);
+        tmp |= horizontal_watch_board & (tmp >> 1);
+        tmp |= horizontal_watch_board & (tmp >> 1);
+        tmp |= horizontal_watch_board & (tmp >> 1);
+        legal_board |= blank_board & (tmp >> 1);
+
+        // 上
+        tmp = vertical_watch_board & (self.player_board << 8);
+        tmp |= vertical_watch_board & (tmp << 8);
+        tmp |= vertical_watch_board & (tmp << 8);
+        tmp |= vertical_watch_board & (tmp << 8);
+        tmp |= vertical_watch_board & (tmp << 8);
+        tmp |= vertical_watch_board & (tmp << 8);
+        legal_board |= blank_board & (tmp << 8);
+
+        // 下
+        tmp = vertical_watch_board & (self.player_board >> 8);
+        tmp |= vertical_watch_board & (tmp >> 8);
+        tmp |= vertical_watch_board & (tmp >> 8);
+        tmp |= vertical_watch_board & (tmp >> 8);
+        tmp |= vertical_watch_board & (tmp >> 8);
+        tmp |= vertical_watch_board & (tmp >> 8);
+        legal_board |= blank_board & (tmp >> 8);
+
+        // 右上
+        tmp = all_side_watch_board & (self.player_board << 7);
+        tmp |= all_side_watch_board & (tmp << 7);
+        tmp |= all_side_watch_board & (tmp << 7);
+        tmp |= all_side_watch_board & (tmp << 7);
+        tmp |= all_side_watch_board & (tmp << 7);
+        tmp |= all_side_watch_board & (tmp << 7);
+        legal_board |= blank_board & (tmp << 7);
+
+        // 左上
+        tmp = all_side_watch_board & (self.player_board << 9);
+        tmp |= all_side_watch_board & (tmp << 9);
+        tmp |= all_side_watch_board & (tmp << 9);
+        tmp |= all_side_watch_board & (tmp << 9);
+        tmp |= all_side_watch_board & (tmp << 9);
+        tmp |= all_side_watch_board & (tmp << 9);
+        legal_board |= blank_board & (tmp << 9);
+
+        // 右下
+        tmp = all_side_watch_board & (self.player_board >> 9);
+        tmp |= all_side_watch_board & (tmp >> 9);
+        tmp |= all_side_watch_board & (tmp >> 9);
+        tmp |= all_side_watch_board & (tmp >> 9);
+        tmp |= all_side_watch_board & (tmp >> 9);
+        tmp |= all_side_watch_board & (tmp >> 9);
+        legal_board |= blank_board & (tmp >> 9);
+
+        // 左下
+        tmp = all_side_watch_board & (self.player_board >> 7);
+        tmp |= all_side_watch_board & (tmp >> 7);
+        tmp |= all_side_watch_board & (tmp >> 7);
+        tmp |= all_side_watch_board & (tmp >> 7);
+        tmp |= all_side_watch_board & (tmp >> 7);
+        tmp |= all_side_watch_board & (tmp >> 7);
+        legal_board |= blank_board & (tmp >> 7);
+
+        legal_board
+    }
+
+    pub fn is_possible(&self, co: Coordinate) -> bool {
+        (Board::coordinate_to_bit(co) & self.make_legal_board()) != 0
+    }
+}
+
+mod tests {
+    use crate::board::Board;
+
+    #[test]
+    fn coordinate_to_bit_test() {
+        let mask = Board::coordinate_to_bit((0, 0));
+
+        assert_eq!(
+            0b_10000000_00000000_00000000_00000000_00000000_00000000_00000000_00000000,
+            mask
+        );
+
+        let mask = Board::coordinate_to_bit((5, 4));
+        assert_eq!(
+            0b_00000000_00000000_00000000_00000000_00000000_00001000_00000000_00000000,
+            mask
+        );
+    }
+
+    #[test]
+    fn make_legal_board_test() {
+        let default_board = Board::new();
+        let legal_board = default_board.make_legal_board();
+
+        assert_eq!(
+            0b_00000000_00000000_00010000_00100000_00000100_00001000_00000000_00000000,
+            legal_board
+        )
+    }
+    
+    #[test]
+    fn is_possible_test(){
+        let default_board=Board::new();
+        assert!(default_board.is_possible((3, 2)));
+        assert!(!default_board.is_possible((0, 0)));
     }
 }
