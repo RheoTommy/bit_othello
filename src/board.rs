b261e2d6ba62e4ab8c675193bd07e18c1887b900
RheoTommy <tommyrheo@gmail.com>
2020-08-07 20:15:14 +0900

    Boardの雑な作成

--- src/board.rs
+++ src/board.rs
@@ -1,30 +1,56 @@
+use crate::board::Player::Black;
 use std::fmt::{Debug, Formatter};
+use std::mem::swap;
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq)]
-pub enum Turn {
+pub enum Player {
     Black,
     White,
+}
+
+impl Player {
+    pub fn next(&self) -> Self {
+        match self {
+            Player::Black => Player::White,
+            Player::White => Player::Black,
+        }
+    }
 }
 
 pub type BitBoard = usize;
 pub type Coordinate = (usize, usize);
 pub type ICoordinate = (isize, isize);
 
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum JudgeResult {
+    Continue,
+    Draw,
+    Win(Player),
+}
+
+#[derive(Copy, Clone, Debug, Eq, PartialEq)]
+pub enum Choice {
+    Skip,
+    Coordinate(Coordinate),
+}
+
 #[derive(Clone, Eq, PartialEq)]
 pub struct Board {
-    pub turn_num: usize,
-    pub turn: Turn,
+    pub turn: usize,
+    pub player: Player,
     pub player_board: BitBoard,
     pub opponent_board: BitBoard,
 }
 
 impl Debug for Board {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
-        let turn = self.turn;
+        let turn = self.player;
         let (black, white) = match turn {
-            Turn::Black => (self.player_board, self.opponent_board),
-            Turn::White => (self.opponent_board, self.player_board),
+            Player::Black => (self.player_board, self.opponent_board),
+            Player::White => (self.opponent_board, self.player_board),
         };
+
+        let dialog = format!("<Turn: {}>\nNext Player: {:?}", self.turn, self.player);
 
         let mut str = " |0|1|2|3|4|5|6|7|\n".to_string();
 
@@ -48,15 +74,15 @@ impl Debug for Board {
             str.push_str("\n");
         }
 
-        write!(f, "{}", str)
+        write!(f, "{}\n{}", dialog, str)
     }
 }
 
 impl Board {
     pub fn new() -> Self {
         Self {
-            turn_num: 1,
-            turn: Turn::Black,
+            turn: 1,
+            player: Player::Black,
             player_board: 0x0000000810000000,
             opponent_board: 0x0000001008000000,
         }
@@ -154,18 +180,66 @@ impl Board {
         legal_board
     }
 
-    pub fn is_possible(&self, co: Coordinate) -> bool {
-        (Board::coordinate_to_bit(co) & self.make_legal_board()) != 0
+    pub fn is_possible(&self, put: BitBoard) -> bool {
+        (put & self.make_legal_board()) != 0
+    }
+
+    fn skip(&mut self) {
+        self.turn += 1;
+        self.player = self.player.next();
+        swap(&mut self.player_board, &mut self.opponent_board)
+    }
+
+    pub fn update(&mut self, choice: Choice) -> Result<JudgeResult, &'static str> {
+        if let Choice::Skip = choice {
+            self.skip();
+            return Ok(JudgeResult::Continue);
+        }
+
+        let co = match choice {
+            Choice::Skip => unreachable!(),
+            Choice::Coordinate(co) => co,
+        };
+
+        let (i, j) = co;
+        let put = Board::coordinate_to_bit(co);
+
+        if !(i < 8 && j < 8) {
+            return Err("Out of index!");
+        }
+
+        if !self.is_possible(put) {
+            return Err("Impposible Choice!");
+        }
+
+        self.reverse(put);
+
+        swap(&mut self.player_board, &mut self.opponent_board);
+        self.player = self.player.next();
+        self.turn += 1;
+
+        if self.is_game_finished() {
+            let (player, opponent) = self.calc_now_score();
+            return if player == opponent {
+                Ok(JudgeResult::Draw)
+            } else if player > opponent {
+                Ok(JudgeResult::Win(self.player))
+            } else {
+                Ok(JudgeResult::Win(self.player.next()))
+            };
+        }
+
+        Ok(JudgeResult::Continue)
     }
 
     fn reverse(&mut self, put: BitBoard) {
         let mut rev = 0;
         for k in 0..8 {
-            let rev_ = 0;
+            let mut rev_ = 0;
             let mut mask = Board::transfer(put, k);
 
             while (mask != 0) && ((mask & self.opponent_board) != 0) {
-                rev |= mask;
+                rev_ |= mask;
                 mask = Board::transfer(mask, k);
             }
 
@@ -176,7 +250,6 @@ impl Board {
 
         self.player_board ^= put | rev;
         self.opponent_board ^= rev;
-        self.turn_num += 1;
     }
 
     fn transfer(put: BitBoard, k: usize) -> BitBoard {
@@ -192,9 +265,47 @@ impl Board {
             _ => unimplemented!(),
         }
     }
+
+    pub fn is_pass(&self) -> bool {
+        let player_legal_board = self.make_legal_board();
+
+        let opponent_board = Self {
+            player: Black,
+            turn: 0,
+            player_board: self.opponent_board,
+            opponent_board: self.player_board,
+        };
+
+        let opponent_legal_board = opponent_board.make_legal_board();
+
+        player_legal_board == 0x0000000000000000 && opponent_legal_board != 0x0000000000000000
+    }
+
+    fn is_game_finished(&self) -> bool {
+        let player_legal_board = self.make_legal_board();
+
+        let opponent_board = Self {
+            player: Black,
+            turn: 0,
+            player_board: self.opponent_board,
+            opponent_board: self.player_board,
+        };
+
+        let opponent_legal_board = opponent_board.make_legal_board();
+
+        player_legal_board == 0x0000000000000000 && opponent_legal_board == 0x0000000000000000
+    }
+
+    pub fn calc_now_score(&self) -> (u32, u32) {
+        let player_num = self.player_board.count_ones();
+        let opponent_num = self.opponent_board.count_ones();
+
+        (player_num, opponent_num)
+    }
 }
 
 mod tests {
+    #[allow(unused_imports)]
     use crate::board::Board;
 
     #[test]
